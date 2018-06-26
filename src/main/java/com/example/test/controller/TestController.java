4efb3d41173b6d4e54ed7d257de6da98a2ed8412
ethan <programcccethan@gmail.com>
2018-06-26 11:22:01 +0800

    Initial commit

---
+++ src/main/java/com/example/test/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.example.test.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by liwenqing on 2018/6/26.
+ */
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public String test() {
+        String a = "ss";
+        return "hello Spring MVC";
+    }
+
+
+}
